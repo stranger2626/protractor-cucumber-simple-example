312ea62581eee5f6518c40cc00a027e263e67701
Матвей Мурашко <matsvei.murashka@wolterskluwer.com>
2020-05-27 02:28:43 +0300

    added html reporter

--- test/config/protractor_config.js
+++ test/config/protractor_config.js
@@ -1,5 +1,13 @@
 const path = require('path');
 const yargs = require('yargs').argv;
+const reporter = require('cucumber-html-reporter');
+
+const reportOtions = {
+  theme: 'bootstrap',
+  jsonFile: path.join(__dirname, '../reports/report.json'),
+  output: path.join(__dirname, '../reports/cucumber-report.html'),
+  reportSuitsAsSCenarios: true
+};
 
 exports.config = {
   allScriptsTimeout: 60000,
@@ -8,6 +16,8 @@ exports.config = {
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
   capabilities: {
+    shardTestFiles: yargs.instances > 1,
+    maxInstances: yargs.instances || 1,
     browserName: 'chrome',
     chromeOptions: {
       args: ['--no-sandbox', '--window-size=1920,1080']
@@ -23,5 +33,8 @@ exports.config = {
   },
   onPrepare: () => {
     return browser.waitForAngularEnabled(false);
+  },
+  afterLaunch: () => {
+    return reporter.generate(reportOtions);
   }
 };
